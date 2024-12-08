dd5ce8811e5901aa4afa52233c6826c1c00bd56e
知鱼 <ovoaa@qq.com>
2024-12-08 18:27:19 +0800

    Update 抖音视频解析.js

--- 抖音视频解析.js
+++ 抖音视频解析.js
@@ -1,9 +1,8 @@
 import common from '../../lib/common/common.js';
 import fetch from "node-fetch";
-//作者：知鱼
-//博客：http://ocoa.cn
 //QQ群：861646887
 //GitHub：https://github.com/ovoox
+//更多JS插件请访问GitHub
 export class VideoParser extends plugin {
     constructor() {
         super({
